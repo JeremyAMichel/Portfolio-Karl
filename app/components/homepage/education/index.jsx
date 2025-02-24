4b827b1b33a7be7c2b767ff67941d2c432097f9e
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2025-02-24 18:17:15 +0100

    Trying to fix Deployment error

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -43,7 +43,7 @@ function Education() {
           </div>
 
           <div>
-            {/* <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6">
               {
                 educations.map(education => (
                   <GlowCard key={education.id} identifier={`education-${education.id}`}>
@@ -75,7 +75,7 @@ function Education() {
                   </GlowCard>
                 ))
               }
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
