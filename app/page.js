5838a79b2b5611023e280f126311147019c4ed8e
Karl2242 <kchikhicloud@gmail.com>
2025-02-24 16:27:20 +0100

    FIX: App/page.js became client component

--- app/page.js
+++ app/page.js
@@ -1,38 +1,39 @@
+'use client';
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { useEffect, useState } from 'react';
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-  const blogs = await getData();
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await res.json();
+      const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+      setBlogs(filtered);
+    }
+    
+    fetchData();
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
       <Skills />
       <Projects />
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
